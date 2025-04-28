9b6762e060fbc41ee7edf0591147330889850414
AxelSantiago17 <axel.santiago1531@gmail.com>
2025-04-28 15:13:05 -0600

    avance

--- AxelSantiago17/main.py
+++ AxelSantiago17/main.py
@@ -1,9 +1,30 @@
-class Perfil:
-    def __init__ (self,x,y,ancho,alto,imagen,velocidad):
-         self.x = X
-         self.y = y
-         self.ancho = ancho
-         self.alto =alto
-         self.imagen = imagen
-         self.velocidad = velocidad
-         
+#1. Importar el framework o paquete
+import pygame
+from pygame.locals import*
+import sys
+
+#2. Definir constantes
+ANCHO = 80
+ALTO = 80
+FPS = 30 
+blanco = (255,255,255)
+
+#3. Inicializar pygame
+pygame.init()
+ventana = pygame.display.set_mode((ANCHO_VENTANA,ALTO_VENTANA))
+reloj = pygame.time.Clock()
+
+#4. Cargar recursos (imagenes)
+#5. Inicializar variables
+
+#6. Ciclo infinito
+    #7. Verificar y manejar los eventos
+    #8. Realizar cualquier acción por frame
+
+    #9. Limpiar la ventana
+
+    #10. Dibujar elementos en la ventana
+    
+    #11. Actualizar la ventana
+    
+    #12. Ralentizar un poco las cosas
