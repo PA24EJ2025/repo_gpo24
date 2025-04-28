6c4d3fdcdb7e18db3c986890482b83ac76d85b17
DamianFTP <angelftp18@gmail.com>
2025-04-28 15:30:57 -0600

    empece el trabajo

---
+++ DamianFTP/main.py
@@ -0,0 +1,38 @@
+class Perfil:
+    def __int__(x,y,ancho,altura,foto,velocidad):
+        self.x = x
+        self.y = y
+        self.ancho = ancho
+        self.altura = altura
+        self.foto = foto
+        self.velocidad = velocidad
+    
+    
+#1. Importar el framework o paquete
+import pygame
+from pygame.locals import*
+import sys
+
+#2. Definir constantes
+alto = 80
+ancho = 80
+Fps = 30
+blanco = (255,255,255)
+#3. Inicializar pygame
+pygame.int()
+ventana = pygame.display.set_mode((ancho_ventana_alto_ventana))
+reloj = pygame.time.Clock()
+#4. Cargar recursos (imagenes)
+#5. Inicializar variables
+
+#6. Ciclo infinito
+    #7. Verificar y manejar los eventos
+    #8. Realizar cualquier acción por frame
+
+    #9. Limpiar la ventana
+
+    #10. Dibujar elementos en la ventana
+    
+    #11. Actualizar la ventana
+    
+    #12. Ralentizar un poco las cosas
