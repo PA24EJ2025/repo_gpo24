903f4e70ab24a74eeefbc1b79dc5b38136f1864f
Bruno Rafel De la Garza Santos <brunorafaeldelagarza@gmail.com>
2025-04-28 18:59:08 -0600

    Crear Clase

--- Bolsadepapus/main.py
+++ Bolsadepapus/main.py
@@ -10,11 +10,29 @@ FPS = 60
 pygame.init()
 
 #4. Cargar recursos (imagenes)
+
 imagen_perfil = pygame.image.load("Bolsadepapus/images.png")
+
 #5. Inicializar variables
 ventana = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
 pygame.display.set_caption("Mover Foto de Perfil")
 reloj = pygame.time.Clock()
+
+# Clase Perfil
+class Perfil:
+    def __init__(self, x, y, alto, ancho, velocidad):
+        self.x = x
+        self.y = y
+        self.alto = alto
+        self.ancho = ancho
+        self.velocidad = velocidad
+        self.imagen = pygame.transform.scale(imagen_perfil, (self.ancho, self.alto))
+
+    def mostrar(self, ventana):
+        ventana.blit(self.imagen, (self.x, self.y))
+
+perfil = Perfil(x=100, y=100, alto=80, ancho=80, velocidad=5)
+
 #6. Ciclo infinito
     #7. Verificar y manejar los eventos
     #8. Realizar cualquier acción por frame
