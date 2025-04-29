b0b5b9e5e58d8733ee398deea241eb1b989a8c1a
Dania Julieta Méndez Guerra <dania.mendezg@uanl.edu.mx>
2025-04-29 13:25:59 -0600

    listo

--- Moon_7/28abril_ev3.py
+++ Moon_7/28abril_ev3.py
@@ -1,11 +1,52 @@
+#EVIDENCIA 3 PROGRAMACION AVANZADA
+
 import pygame
 from pygame.locals import *
-import sys
 import perfil
+import sys
 
-ANCHO_VENTANA=
-ALTP_VENTANA=
-FPS=30
-SIZE_CUADRO=80
-ANCHO_MAX=ANCHO_VENTANA-SIZE_MAX
 
+ALTO_VENTANA=500
+ANCHO_VENTANA=700
+MORADO = (182, 149, 192)
+FPS=40
+SIZE_CUADRO=70
+ANCHO_MAX=ANCHO_VENTANA-SIZE_CUADRO
+ALTO_MAX=ALTO_VENTANA-SIZE_CUADRO
+
+pygame.init()
+ventana = pygame.display.set_mode((ANCHO_VENTANA,ALTO_VENTANA))
+reloj = pygame.time.Clock()
+
+class perfil:
+  def __init__(self,x,y,alto,ancho,ruta_imagen):
+     self.x=x
+     self.y=y
+     self.alto=alto
+     self.anncho=ancho
+     self.imagen = pygame.image.load(ruta_imagen)
+     
+def mover(self,ventana):
+  ventana.blit(self.imagen,
+               (self.x,self.y,
+               self.ancho,self.alto))
+    
+def dibujar(self,ventana):
+    ventana.blit(self.imagen,
+                 (self.x,self.y,
+                  self.ancho,self.alto))
+    
+imagen=perfil(90,90,60,60, 'Moon_7/perfil.png')
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    obj_perfil =perfil.perfil("Moon",40,40,70,70)
+    obj_perfil.dibujar(ventana)
+    
+    pygame.display.update()
+    reloj.tick(FPS) 
+    
