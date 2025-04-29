a5dae404ce540628ca44ece92e398075d2fdc965
AngelDaelBautistaResendiz <resendizdael@gmail.com>
2025-04-28 22:12:40 -0600

    actualizado modulo.py

--- AngelDaelBautistaResendiz/modulo.py
+++ AngelDaelBautistaResendiz/modulo.py
@@ -7,8 +7,8 @@ class Perfil:
         self.y = y
         self.ancho = 80
         self.alto = 80
-        self.imagen = pygame.image.load("fotoperfil.png")
+        self.imagen = pygame.image.load("fotoperfil.png") 
         self.velocidad = velocidad
     
     def dibujar(self, ventana):
-        ventana.blit(self.imagen, (self.x, self.y))
+        ventana.blit(self.imagen, (self.x, self.y)) 
